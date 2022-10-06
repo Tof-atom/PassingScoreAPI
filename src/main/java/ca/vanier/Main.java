a5b5395077a6c5d32e582260f41f0b8957b633bd
Tof-atom <toufel@inbox.ru>
2022-10-05 20:59:34 -0400

    last commit

--- src/main/java/ca/vanier/Main.java
+++ src/main/java/ca/vanier/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         ScoreManager sc = new ScoreManager();
 
-        int score = 7;
+        int score = 8;
 
         System.out.println(sc.Check(score));
     }
